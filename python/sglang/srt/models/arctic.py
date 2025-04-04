da01934876808f5cd813e6747464ef9144ec74b6
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-04-04 09:13:00 -0400

    feat: Add arctic model definition
    feat: Implement ArcticForCausalLM model class
    
    fix: Add deepspeed requirement comment to ArcticForCausalLM
    
    refactor: Add reminder comment for ArcticForCausalLM dependencies
    
    feat: Implement ArcticForCausalLM init method
    
    This commit implements the `__init__` method for the `ArcticForCausalLM` class. It initializes the model with a given configuration, quantization configuration, and prefix. It also initializes the language model head and logits processor. Additionally, it adds the Apache 2.0 license header from Hugging Face.
    
    feat: Add canned imports and deepspeed MoE import for ArcticForCausalLM
    
    This commit adds a set of canned imports from the transformers library that are likely to be used in the ArcticForCausalLM implementation. It also includes a conditional import of the MoE layer from deepspeed, ensuring that it's only imported if deepspeed is available. This is necessary because the Arctic model may depend on deepspeed for Mixture of Experts (MoE) functionality.

---
+++ python/sglang/srt/models/arctic.py
@@ -0,0 +1,101 @@
+# coding=utf-8
+# Copyright 2023 Mistral AI and the HuggingFace Inc. team. All rights reserved.
+#
+# This code is based on EleutherAI's GPT-NeoX library and the GPT-NeoX
+# and OPT implementations in this library. It has been modified from its
+# original forms to accommodate minor architectural differences compared
+# to GPT-NeoX and OPT used by the Meta AI team that trained the model.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Optional, List
+
+import torch
+from torch import nn
+from transformers import PretrainedConfig
+
+from sglang.srt.layers.logits_processor import LogitsProcessor
+from sglang.srt.layers.quantization.base_config import QuantizationConfig
+from sglang.srt.layers.radix_attention import RadixAttention
+from sglang.srt.layers.vocab_parallel_embedding import ParallelLMHead, VocabParallelEmbedding
+from sglang.srt.model_executor.forward_batch_info import ForwardBatch
+from sglang.srt.model_loader.weight_utils import default_weight_loader
+from sglang.srt.utils import add_prefix
+
+# canned imports, should be removed after a while
+import deepspeed
+import torch
+import torch.nn.functional as F
+import torch.utils.checkpoint
+from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+
+from transformers.activations import ACT2FN
+from transformers.cache_utils import Cache, DynamicCache
+from transformers.modeling_attn_mask_utils import (
+    _prepare_4d_causal_attention_mask,
+    _prepare_4d_causal_attention_mask_for_sdpa,
+)
+from transformers.modeling_outputs import (
+    MoeCausalLMOutputWithPast,
+    MoeModelOutputWithPast,
+    SequenceClassifierOutputWithPast,
+)
+from transformers.modeling_utils import PreTrainedModel
+from transformers.pytorch_utils import is_torch_greater_or_equal_than_1_13
+from transformers.utils import (
+    add_start_docstrings,
+    add_start_docstrings_to_model_forward,
+    is_flash_attn_2_available,
+    is_flash_attn_greater_or_equal_2_10,
+    logging,
+    replace_return_docstrings,
+)
+from transformers.utils.import_utils import is_torch_fx_available
+from .configuration_arctic import ArcticConfig
+from transformers.integrations.deepspeed import is_deepspeed_available
+from transformers.utils.versions import require_version
+# canned imports, should be removed after a while
+# requires deepspeed as required features....
+# reminder to add the arctic specific deps and change all and eev options to have the deepspeed 0.14.2 >= dep.
+
+# key points:
+class ArcticForCausalLM(nn.Module):
+  def __init__(
+        self,
+        config: PretrainedConfig,
+        quant_config: Optional[QuantizationConfig] = None,
+        prefix: str = "",
+    ):
+        super().__init__()
+        self.config = config
+        self.model = ArcticForCausalLM(config, quant_config, prefix=add_prefix("model", prefix))
+        self.lm_head = ParallelLMHead(
+            config.vocab_size,
+            config.hidden_size,
+            quant_config=quant_config,
+            prefix=add_prefix("lm_head", prefix),
+        )
+        self.logits_processor = LogitsProcessor(config)
+
+# copied from the original
+# note that:
+# the check for deepspeed is important (perhaps it can go in util). will NOT WORK WITHOUT IT!
+if is_deepspeed_available():
+    from deepspeed.moe.layer import MoE 
+    # Note that below will crash if there is an available deepspeed that does not have ds_linear.
+    try:
+        import deepspeed.linear as ds_linear
+    except Exception:
+        pass
+else:
+    MoE = None
